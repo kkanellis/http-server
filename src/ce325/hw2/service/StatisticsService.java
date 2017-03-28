f9385f5312852f8afc11166cbf2a52599fbd7d0f
George Gougoudis <george_gougoudis@hotmail.com>
2017-03-28 12:45:30 +0300

    Implement the statistics service, needs testing

--- src/ce325/hw2/service/StatisticsService.java
+++ src/ce325/hw2/service/StatisticsService.java
@@ -1,7 +1,10 @@
 package ce325.hw2.service;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -15,32 +18,93 @@ public class StatisticsService {
         return ourInstance;
     }
 
+    private final static int MAX_MEAN_SLOTS = 10;
+
     private long mStarted;
     private AtomicLong mConnections;
-    private AtomicInteger mMeanCounter;
-    private AtomicLong mServiceTime;
-
+    private AtomicInteger mMeanTime;
+    private AtomicIntegerArray mTimeArray;
 
     private StatisticsService() {
-        mServiceTime.set(0);
-        mConnections.set(0);
-        mMeanCounter.set(0);
+        mConnections = new AtomicLong(0);
+        mMeanTime = new AtomicInteger(0);
+        mTimeArray = new AtomicIntegerArray(MAX_MEAN_SLOTS);
     }
 
     public void start() {
         mStarted = System.currentTimeMillis();
     }
 
-    public void addConnection() {
-        mConnections.incrementAndGet();
+    public int getTimeDelta() {
+        return (int)(mStarted - System.currentTimeMillis());
     }
 
+    /**
+     * Called when a thread starts serving an exchange
+     * @param threadId the thread id, negative integer
+     * @return the time slot if one is free, or -1 if not
+     */
+    public int onConnect(int threadId){
+        if (threadId >= 0) {
+            throw new IllegalArgumentException("threadId must be negative!");
+        }
+        // increment and get total connections
+        long connections = mConnections.incrementAndGet();
+        int sum = 0;
+        int nonZero = 0;
+
+        // Calculate the sum, and try to zero out the slots
+        if ((connections % MAX_MEAN_SLOTS) == 0) {
+            for (int i = 0; i < mTimeArray.length(); i++) {
+                int time = mTimeArray.get(i);
+                if (time > 0) {
+                    // Zero the time, only if it wasn't modified
+                    mTimeArray.compareAndSet(i, time, 0);
+                    sum += time;
+                    nonZero++;
+                }
+            }
+        }
+
+        // calculate the mean time
+        mMeanTime.set(sum/nonZero);
+
+        // try to get a slot for mean request time calculation
+        for (int i=0; i < mTimeArray.length(); i++) {
+            if (mTimeArray.compareAndSet(i, 0, threadId)) {
+                // we managed to get a slot
+                return i;
+            }
+        }
+        // All slots are full
+        // TODO: log this
+        System.err.println("All time slots are full :(");
+        return -1;
+    }
+
+    /**
+     * Set the request time for this slot
+     * @param slot the time slot for this exchange
+     * @param time the time this exchange took
+     */
+    public void onDisconnect(int slot, int time) {
+        mTimeArray.set(slot, time);
+    }
+
+    /**
+     * Get the total connections
+     * @return total connections
+     */
     public long getConnections() {
         return mConnections.get();
     }
 
-    public void addServiceTime(int time) {
-        mServiceTime.getAndAdd(time);
+    /**
+     * Get the mean request serve time
+     * @return the mean time
+     */
+    public int getMeanTime() {
+        return mMeanTime.get();
     }
 
 }
