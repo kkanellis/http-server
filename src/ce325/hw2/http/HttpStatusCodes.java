7458faf3332aaf2a1708e3cefd63db6227203b63
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-04-20 23:19:18 +0300

    Add HTTP Status Codes class

---
+++ src/ce325/hw2/http/HttpStatusCodes.java
@@ -0,0 +1,12 @@
+package ce325.hw2.http;
+
+/**
+ * Created by sfi on 20/4/2017.
+ */
+public class HttpStatusCodes {
+    public static final int HTTP_OK = 200;
+    public static final int HTTP_BAD_REQUEST = 400;
+    public static final int HTTP_NOT_FOUND = 404;
+    public static final int HTTP_METHOD_NOT_ALLOWED = 405;
+    public static final int HTTP_SERVER_ERROR = 500;
+}
